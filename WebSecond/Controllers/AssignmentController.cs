5d8ef5ab5abf7a1cf3bd0a07177bdb7a4904a48c
hpinkmi <hpinkmi@gmail.com>
2019-10-07 16:49:18 -0500

    firstassignment

---
+++ WebSecond/Controllers/AssignmentController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+public class AssignmentContainer
+{
+    public string user { get; set; }
+    public int count { get; set; }
+}
+
+namespace WebSecond.Controllers
+{
+    public class AssignmentController : Controller
+ 
+    {
+        // GET: Assignment
+        public ActionResult assign()
+        {
+            return View();
+        }
+        public ActionResult test(AssignmentContainer data)
+        {
+            return View("assign", data);
+        }
+    }
+}
---
+++ WebSecond/Controllers/DefaultController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+public class container
+{
+   public string id { get; set; }
+   public string user { get; set; }
+   public string pw { get; set; }
+}
+
+namespace WebSecond.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index(string id)
+        {
+            return View((object)id);
+        }
+
+        public ActionResult Login(container data)
+        {
+            return View("Index",data);
+        }
+    }
+}
